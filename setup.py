61981947a8d4d6c04f326a63471af1ef71f69c85
giulia-mmp <108267064+giulia-mmp@users.noreply.github.com>
2022-06-29 20:24:28 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+from mmp_theme import __version__
+
+setup(
+    name='mmp_theme',
+    version=__version__,
+    description='Custom theme and functions for MMP plots',
+
+    url='https://source.cloud.google.com/mmp-vm/utility_repo/+/master:mmp_theme/',
+    author='Giulia Bortolato',
+    author_email='giulia.bortolato@milanomultiphysics.com',
+
+    packages=find_packages()
+)
