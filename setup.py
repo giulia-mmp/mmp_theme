f060b4635afd38b6750d42b38b797793e15a359b
Giulia <giulia.bortolato@milanomultiphysics.com>
2022-06-30 12:57:36 +0200

    Accessory files for packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
+#from mmp_theme import __version__
 
-from mmp_theme import __version__
+# Reads the content of your README.md into a variable to be used in the setup below
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='mmp_theme',
-    version=__version__,
+    version='1.1',
+    packages=find_packages(),
+    license='MIT',
     description='Custom theme and functions for MMP plots',
-
-    url='https://source.cloud.google.com/mmp-vm/utility_repo/+/master:mmp_theme/',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
     author='Giulia Bortolato',
     author_email='giulia.bortolato@milanomultiphysics.com',
-
-    packages=find_packages()
+    url='https://source.cloud.google.com/mmp-vm/utility_repo/+/master:mmp_theme/',
+    install_requires=['requests'],
+    
+    #download_url=''
+    
 )
