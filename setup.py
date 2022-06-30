84b7454b5f31b2b6adb8ba5e7fe1d4b99376cdfe
Giulia <giulia.bortolato@milanomultiphysics.com>
2022-06-30 13:06:41 +0200

    Bug fixed in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+import setuptools
 #from mmp_theme import __version__
 
 # Reads the content of your README.md into a variable to be used in the setup below
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='mmp_theme',
     version='1.1',
-    packages=find_packages(),
+    packages=['mmp_theme'],
     license='MIT',
     description='Custom theme and functions for MMP plots',
     long_description=long_description,
@@ -18,6 +18,6 @@ setup(
     url='https://source.cloud.google.com/mmp-vm/utility_repo/+/master:mmp_theme/',
     install_requires=['requests'],
     
-    #download_url=''
+    download_url="https://github.com/mike-huls/toolbox_public/archive/refs/tags/v1.1.tar.gz"
     
 )
