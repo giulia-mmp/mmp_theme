8d0fe8538228396cc8fc627c8b8df97bd85f103a
Giulia <giulia.bortolato@milanomultiphysics.com>
2022-11-21 16:44:31 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
     url='https://source.cloud.google.com/mmp-vm/utility_repo/+/master:mmp_theme/',
     install_requires=['requests'],
     
-    download_url="https://github.com/giulia-mmp/mmp_theme/archive/refs/tags/v1.2.1.tar.gz"
+#    download_url="https://github.com/giulia-mmp/mmp_theme/archive/refs/tags/v1.2.1.tar.gz"
+    download_url="https://github.com/giulia-mmp/mmp_theme/archive/refs/tags/v2.0.tar.gz"
     
 )
