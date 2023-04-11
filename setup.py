29ad554dd68bd725fc79d4120e711069949aac55
Giulia <giulia.bortolato@milanomultiphysics.com>
2023-04-11 14:38:39 +0200

    changed exponentformat in theme

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mmp_theme',
-    version='2.0.1',
+    version='2.0.2',
     packages=['mmp_theme'],
     license='MIT',
     description='Custom theme and functions for MMP plots',
